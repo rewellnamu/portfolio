bd6ffe7af5165c6b40dd23502f00d2448e73316f
rewell namu <riwenamu@gmail.com>
2025-05-17 12:59:13 +0300

    chore: migrate testing framework from Karma/Jasmine to Jest
    - Updated package.json to replace Karma/Jasmine with Jest for testing.
    - Added jest and jest-preset-angular dependencies.
    - Created jest.config.js to configure Jest for Angular testing.
    - Added setup-jest.ts for Jest setup.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|mjs|html|js)$': 'jest-preset-angular',
+  },
+};
