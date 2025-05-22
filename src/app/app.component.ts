52e68a22b5f1f66e3838b0f43b02443c6a24068b
rewell namu <riwenamu@gmail.com>
2025-05-22 13:28:10 +0300

    refactor: update home component tagline for clarity; enhance app structure with additional components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from "./components/about/about.component";
+import { CertificatesComponent } from './components/certificates/certificates.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, AboutComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, ResumeComponent, SkillsComponent, ProjectsComponent, ContactComponent, CertificatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
