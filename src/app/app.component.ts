fd8575d0533235778c3880b84785c69397074c03
rewell namu <riwenamu@gmail.com>
2025-05-26 09:45:14 +0300

    refactor: remove unused components from app structure for improved clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from "./components/about/about.component";
-import { CertificatesComponent } from './components/certificates/certificates.component';
+
 import { ContactComponent } from './components/contact/contact.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, AboutComponent, ResumeComponent, SkillsComponent, ProjectsComponent, ContactComponent, CertificatesComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
