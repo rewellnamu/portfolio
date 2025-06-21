8829fb141c567dfebb504e671410ea8f5c4670a2
rewell namu <riwenamu@gmail.com>
2025-06-21 16:27:21 +0300

    changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [ RouterModule ],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  scrollingText = '🤾‍♂️🤾‍♂️🚲';
 }
